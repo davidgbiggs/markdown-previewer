b998983e926b742137edd3916bf0132ca6b051e8
David Biggs <biggs.dg@gmail.com>
2021-03-17 20:31:24 -0500

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.scss";
 
 function App() {
